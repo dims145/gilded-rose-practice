71923a94c9fc474be82a0f5b78f9524069ab28c7
dims145 <d.wahyu1454@gmail.com>
2026-05-01 14:53:17 +0700

    Update test edge cases to maximize coverage

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -41,68 +41,90 @@ class GildedRoseTest {
 
     @Test
     void givenBackstagePassesItem_when50DaysPassed_shouldCalculateCorrectQuality() {
-        Item[] items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", 15, 15)};
+        Item[] items =
+                new Item[] {
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 15),
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 45),
+                };
 
         GildedRose app = new GildedRose(items);
         assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
-        Item item = app.items[0];
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(10, item.sellIn);
-        assertEquals(20, item.quality);
+        assertEquals(10, items[0].sellIn);
+        assertEquals(10, items[1].sellIn);
+        assertEquals(20, items[0].quality);
+        assertEquals(50, items[1].quality);
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(5, item.sellIn);
-        assertEquals(30, item.quality);
+        assertEquals(5, items[0].sellIn);
+        assertEquals(5, items[1].sellIn);
+        assertEquals(30, items[0].quality);
+        assertEquals(50, items[1].quality);
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(0, item.sellIn);
-        assertEquals(45, item.quality);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(0, items[1].sellIn);
+        assertEquals(45, items[0].quality);
+        assertEquals(50, items[1].quality);
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(-5, item.sellIn);
-        assertEquals(0, item.quality);
+        assertEquals(-5, items[0].sellIn);
+        assertEquals(-5, items[1].sellIn);
+        assertEquals(0, items[0].quality);
+        assertEquals(0, items[1].quality);
 
         for (int i = 0; i < 30; i++) {
             app.updateQuality();
         }
-        assertEquals(-35, item.sellIn);
-        assertEquals(0, item.quality);
+        assertEquals(-35, items[0].sellIn);
+        assertEquals(-35, items[1].sellIn);
+        assertEquals(0, items[0].quality);
+        assertEquals(0, items[1].quality);
     }
 
     @Test
     void givenSulfurasItem_when50DaysPassed_shouldCalculateCorrectQuality() {
-        Item[] items = new Item[] {new Item("Sulfuras, Hand of Ragnaros", 15, 80)};
+        Item[] items =
+                new Item[] {
+                    new Item("Sulfuras, Hand of Ragnaros", 15, 80),
+                    new Item("Sulfuras, Hand of Ragnaros", -5, 80)
+                };
 
         GildedRose app = new GildedRose(items);
         assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
-        Item item = app.items[0];
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(15, item.sellIn);
-        assertEquals(80, item.quality);
+        assertEquals(15, app.items[0].sellIn);
+        assertEquals(-5, app.items[1].sellIn);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(80, app.items[1].quality);
 
         for (int i = 0; i < 5; i++) {
             app.updateQuality();
         }
-        assertEquals(15, item.sellIn);
-        assertEquals(80, item.quality);
+        assertEquals(15, app.items[0].sellIn);
+        assertEquals(-5, app.items[1].sellIn);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(80, app.items[1].quality);
 
         for (int i = 0; i < 40; i++) {
             app.updateQuality();
         }
-        assertEquals(15, item.sellIn);
-        assertEquals(80, item.quality);
+        assertEquals(15, app.items[0].sellIn);
+        assertEquals(-5, app.items[1].sellIn);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(80, app.items[1].quality);
     }
 
     @Test
